06d0f29aa9aa2832e0f220da83edd09ae6d05d43
Scott Haney <scotthaney323@gmail.com>
2022-11-29 18:37:30 -0500

    Add a couple more tests

--- WordCounter.Tests/WordCounterTests.cs
+++ WordCounter.Tests/WordCounterTests.cs
@@ -6,7 +6,7 @@ namespace WordCounter.Tests
     public class Tests
     {
         [Test]
-        public void Counts_Words_In_A_Simple_Sentence_That_Has_Only_Letters()
+        public void Counts_Words_When_There_Are_Only_Letters()
         {
             string text = "Hello World";
 
@@ -16,6 +16,30 @@ namespace WordCounter.Tests
             Assert.AreEqual(2, results.Count);
             Assert.AreEqual(1, results["Hello"]);
             Assert.AreEqual(1, results["World"]);
+        }
+
+        [Test]
+        public void Ignores_Case_When_Counting_Words()
+        {
+            string text = "Hello heLlO";
+
+            var wordCounter = CreateWordCounter();
+            var results = wordCounter.Count(text);
+
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual(2, results["Hello"]);
+        }
+
+        [Test]
+        public void Counts_Words_When_There_Is_External_Punctuation()
+        {
+            string text = "hello.hello?hello!hello;hello: hello";
+
+            var wordCounter = CreateWordCounter();
+            var results = wordCounter.Count(text);
+
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual(6, results["hello"]);
         }
 
         private WordCounter CreateWordCounter()
