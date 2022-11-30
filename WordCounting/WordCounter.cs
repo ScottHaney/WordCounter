6d1cfb1c024a9b5f793ca317e39627177bbfefe0
Scott Haney <scotthaney323@gmail.com>
2022-11-29 23:59:23 -0500

    Separate out the word parsing logic to another class

--- WordCounting/WordCounter.cs
+++ WordCounting/WordCounter.cs
@@ -11,7 +11,7 @@ namespace WordCounting
     /// </summary>
     public class WordCounter : IWordCounter
     {
-        private readonly ICharacterIdentifier _characterIdentifier;
+        private readonly WordParser _wordParser;
         private readonly IWordCountMethod _wordCountMethod;
         private readonly bool _mergeCounts;
 
@@ -25,7 +25,7 @@ namespace WordCounting
             IWordCountMethod wordCountMethod = null,
             bool mergeCounts = false)
         {
-            _characterIdentifier = characterIdentifier ?? new CharacterIdentifier();
+            _wordParser = new WordParser(characterIdentifier ?? new CharacterIdentifier());
             _wordCountMethod = wordCountMethod ?? new DefaultWordCountMethod();
             _mergeCounts = mergeCounts;
         }
@@ -61,40 +61,17 @@ namespace WordCounting
             return results;
         }
 
-        private Dictionary<string, int> CountInternal(string text, Dictionary<string, int> currentResults)
+        private Dictionary<string, int> CountInternal(string text, Dictionary<string, int> results)
         {
-            text = text ?? string.Empty;
-
-            int? startIndex = null;
-            var results = currentResults ?? new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
-
-            for (int i = 0; i < text.Length; i++)
-            {
-                var currentChar = text[i];
-                if (_characterIdentifier.IsWordCharacterThatCanStartAWord(currentChar))
-                {
-                    if (startIndex == null)
-                        startIndex = i;
-                }
-                else if (_characterIdentifier.IsWordCharacterThatCantStartAWord(currentChar))
-                { }
-                else if (startIndex != null)
-                {
-                    AddWord(text, startIndex.Value, i - 1, results);
-                    startIndex = null;
-                }
-            }
-
-            //Make sure to the last word at the end of the text in case there is no ending punctuation
-            if (startIndex != null)
-                AddWord(text, startIndex.Value, text.Length - 1, results);
+            results = results ?? new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            foreach (var word in _wordParser.Parse(text))
+                AddWord(word, results);
 
             return results;
         }
 
-        private void AddWord(string text, int startIndex, int endIndex, Dictionary<string, int> results)
+        private void AddWord(string word, Dictionary<string, int> results)
         {
-            var word = text.Substring(startIndex, endIndex - startIndex + 1);
             if (results.TryGetValue(word, out var match))
                 results[word] = _wordCountMethod.UpdateCount(match);
             else
